b86f8533091f648497f8c680d44bbf66f69412ea
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-10-06 13:37:45 +1000

    Added a basic enemy and debree object.
    Debree travels in a straight line to the left.
    Enemies move towards an object tagged "Player", while also being pushed to the left.

---
+++ Assets/Seek.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seek : MonoBehaviour {
+	public GameObject Target;
+	public float MoveSpeed;
+	Vector3 Force = Vector3.zero;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		Target = GameObject.FindGameObjectWithTag ("Player");
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Target != null) {
+			Vector3 tempVec = rb.position - Target.transform.position;
+			tempVec.Normalize ();
+			Force += -(tempVec);
+		}
+		Force.Normalize ();
+		rb.AddForce((Force * MoveSpeed)*Time.deltaTime);
+		Force = Vector3.zero;
+	}
+}
