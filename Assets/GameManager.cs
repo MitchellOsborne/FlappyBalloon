29f7b38acc203265cbb2c3e24d3ab511f966b1a9
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-10-06 20:37:33 +1100

    Added the Game Manager,
    Spawns Enemies after n seconds
    Spawns Debris after n seconds (Derp, Ive misspelt debris in the code)
    Added a KillTimer component, which deletes a gameobject after n seconds of being alive.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+
+	public GameObject EnemyPrefab;
+	public GameObject DebreePrefab;
+
+	public GameObject GameCamera;
+	
+	public float EnemySpawnRate = 0;
+	public float DebreeSpawnRate = 0;
+	private float EnemySpawnCounter = 0;
+	private float DebreeSpawnCounter = 0;
+	// Use this for initialization
+	void Start () {
+		Random.seed = (int)System.DateTime.Today.Ticks;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DebreeSpawnCounter += Time.deltaTime;
+		EnemySpawnCounter += Time.deltaTime;
+
+
+		if (DebreeSpawnCounter > DebreeSpawnRate) {
+			GameObject tempObj = (GameObject)GameObject.Instantiate(DebreePrefab, new Vector2(GameCamera.transform.position.x + 12, GameCamera.transform.position.y + Random.Range (-3, 6)),Quaternion.identity);
+			tempObj.transform.parent = gameObject.transform;
+			DebreeSpawnCounter = 0;
+		}
+
+		if (EnemySpawnCounter > EnemySpawnRate) {
+			GameObject tempObj = (GameObject)GameObject.Instantiate(EnemyPrefab, new Vector2(GameCamera.transform.position.x + 12, GameCamera.transform.position.y + Random.Range (-3, 6)),Quaternion.identity);
+			tempObj.transform.parent = gameObject.transform;
+			EnemySpawnCounter = 0;
+		}
+	}
+}
---
+++ Assets/KillTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillTimer : MonoBehaviour {
+
+	public float LifeTime = 0;
+	private float LifeCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LifeCounter += Time.deltaTime;
+		if (LifeCounter > LifeTime) {
+			Destroy (gameObject);
+		}
+	}
+}
