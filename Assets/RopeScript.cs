6fcc435179bcc0e77d4dbe04840c5b247e182fb1
Mitchell Osborne <MOsborne@live.com.au>
2015-10-13 21:05:41 -0700

    Converted Enemies, Debree and Player to 2D. Added placeholder images for Enemy, Debree and Player/Balloon Sprites. Introduced Player Prefab to main game scene.

---
+++ Assets/RopeScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeScript : MonoBehaviour {
+	public LineRenderer lr;
+	public GameObject balloon;
+	public GameObject man;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lr.SetPosition (0, balloon.transform.position);
+		lr.SetPosition (1, man.transform.position);
+	}
+}
--- Assets/Seek.cs
+++ Assets/Seek.cs
@@ -4,23 +4,23 @@ using System.Collections;
 public class Seek : MonoBehaviour {
 	public GameObject Target;
 	public float MoveSpeed;
-	Vector3 Force = Vector3.zero;
-	Rigidbody rb;
+	Vector2 Force = Vector2.zero;
+	Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
 		Target = GameObject.FindGameObjectWithTag ("Player");
-		rb = gameObject.GetComponent<Rigidbody> ();
+		rb = gameObject.GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Target != null) {
-			Vector3 tempVec = rb.position - Target.transform.position;
+			Vector2 tempVec = rb.position - new Vector2(Target.transform.position.x, Target.transform.position.y);
 			tempVec.Normalize ();
 			Force += -(tempVec);
 		}
 		Force.Normalize ();
 		rb.AddForce((Force * MoveSpeed)*Time.deltaTime);
-		Force = Vector3.zero;
+		Force = Vector2.zero;
 	}
 }
