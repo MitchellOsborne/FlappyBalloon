aceff6fb5f42db4fe5e23edec7947d431a9e430a
Mitchell Osborne <MOsborne@live.com.au>
2015-10-27 21:06:15 -0700

    PARALLAX!!!!!!!!!!

---
+++ Assets/ParallaxScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScript : MonoBehaviour {
+	public Vector3 ResetPos; //The position at when the bounces back
+	public Vector3 NewPos; //The position the object bounces back to
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (transform.position.x < ResetPos.x) {
+			float diff = transform.position.x - ResetPos.x;
+			Vector3 diffVec = new Vector3(diff,0,0);
+			transform.position = NewPos + diffVec;
+		}
+	}
+}
